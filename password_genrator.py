749d7e48abc7f5811ca76797a96e74cafae5a27d
Shubham Bavskar <83008127+shubhambavskar1997@users.noreply.github.com>
2022-10-31 22:11:25 +0530

    initial commit for jenkins Demo

---
+++ password_genrator.py
@@ -0,0 +1,31 @@
+#Password Generator Project
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n")) 
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#define a empty string
+# c_list = ""
+
+#define a empty list
+password=[]
+for count in range(1,nr_letters+1):
+    password.append(random.choice(letters))
+
+for count in range(1,nr_numbers+1):
+    password.append(random.choice(numbers))
+
+for count in range(1,nr_symbols+1):
+    password.append(random.choice(symbols))
+
+random.shuffle(password)
+final_password = ""
+for item in password:
+    final_password += str(item)
+print(final_password)
